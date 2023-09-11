7171fb8e9e3dc7dfa7a6b1e6081847abe433ef28
tomleblais <leblaistom@gmail.com>
2023-09-11 20:31:40 +0200

    `getCountdowHTML` function added + `startTimer` function added

--- js/script.js
+++ js/script.js
@@ -8,19 +8,52 @@ let workDuration = 1500
  */
 let breakDuration = 300
 
+/**
+ * Temps de travail restant en seconde
+ */
 let workCountdown = workDuration
+
+/**
+ * Temps de pause restant en seconde
+ */
 let breakCountdown = breakDuration
 
-let intervalID = setInterval(() => {
-    if (workCountdown > 0) {
-        workCountdown--
-    } else {
-        if (breakCountdown > 0) {
-            breakCountdown--
+/**
+ * Identifiant du timer
+ */
+let intervalID;
+
+/**
+ * Lance le timer
+ */
+function startTimer() {
+    intervalID = setInterval(() => {
+        if (workCountdown > 0) {
+            workCountdown--
         } else {
-            workCountdown = workDuration
-            breakCountdown = breakDuration
+            if (breakCountdown > 0) {
+                breakCountdown--
+            } else {
+                workCountdown = workDuration
+                breakCountdown = breakDuration
+            }
         }
-    }
-    console.log(workCountdown > 0 ? "Travail : " + showTime(workCountdown): "Pause : " + breakCountdown)
-}, 1000)
+        console.log(workCountdown > 0 ? "Travail : " + getCountdownHTML(workCountdown) : "Pause : " + getCountdownHTML(breakCountdown))
+    }, 1000)
+}
+startTimer()
+
+/**
+ * 
+ * @param {number} countdown Temps restant
+ * @returns Temps restant en format `mm:ss`
+ */
+function getCountdownHTML(countdown) {
+    let minutes = Math.trunc(countdown / 60)
+    let seconds = Math.trunc(countdown % 60)
+
+    let minutesString = `<span id="minutes"> ${minutes} </span>`
+    let secondsString = `<span id="seconds"> ${seconds.toString().padStart(2, "0")} </span>`
+
+    return `${minutesString} : ${secondsString}`
+}
